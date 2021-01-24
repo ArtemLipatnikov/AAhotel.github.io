6b72ecf5cc32500fa08fc7ca048ea7332a2cdfb8
ArtemLipatnikov <thesartli@gmail.com>
2021-01-24 22:29:14 +0300

    added advantage, appartments, promotions. footer

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,26 @@ $(function () {
     defaultDate: "today",
   });
 
+  new Swiper('.promotions__items', {
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      820: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      }
+    },
+
+    loop: true,
+    navigation: {
+      prevEl: '.promotions__button-prev',
+      nextEl: '.promotions__button-next'
+    },
+
+  });
+
 });
